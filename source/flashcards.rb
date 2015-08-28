2e9707fa25344a83edfaa5d852b003f54a677769
Domingo Moronta & Angelita Graves <pair+dj-m.gravesal@devbootcamp.com>
2015-08-28 16:35:16 -0400

    implented and tested is_correct?

--- source/flashcards.rb
+++ source/flashcards.rb
@@ -1,25 +1,29 @@
 # Main file for
 class Card
   attr_reader :definition, :answer
+  #attr_accessor :correct_answer
 
   def initialize(cards = {})
     @definition = cards[:definition]
     @answer = cards[:answer]
+    #@correct_answer = cards[:correct_answer]
+  end
+
+  def is_correct?(user_response)
+    if user_response == @answer
+      true
+    else
+      false
+    end
   end
 
   def to_s
-    "#{definition} and #{answer}"
+    "#{definition}" #and #{answer}"
   end
 
   # def create_card
   #   Parser.get_cards_from_file("flashcard_samples.txt")
   # end
+
+
 end
-
-
-#to_s method will
-
-#puts Parser.get_cards_from_file("flashcard_samples.txt")
-# card_info = {:definition => "Describe some crazy thing", :answer => "this is crazy"}
-# test_card = Card.new(card_info)
-# puts test_card
--- spec/flashcard_spec.rb
+++ spec/flashcard_spec.rb
@@ -11,8 +11,28 @@ describe Card do
       expect(card).to be_a_kind_of(Card)
     end
 
+    it "should initialize as a kind of hash" do
+      expect(card_info).to be_a_kind_of(Hash)
+    end
+
     it "it should have the proper attributes" do
       expect(card).to have_attributes(:definition => "Describe some crazy thing", :answer => "this is crazy")
+    end
+  end
+
+  describe "#to_s" do
+    it "should create a string of the definition" do
+      expect(card.to_s).to eq("Describe some crazy thing")
+    end
+  end
+
+  describe "#is_correct?" do
+    it "should return true if the user gives a correct answer" do
+      expect(card.is_correct?("this is crazy")).to eq(true)
+    end
+
+    it "should return false if the user gives an incorrect answer" do
+      expect(card.is_correct?("this ain't crazy")).to eq(false)
     end
 
   end
