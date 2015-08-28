c126ea0119ef816d5c487d035b5f9bd3d50ea6e7
Domingo Moronta & Angelita Graves <pair+dj-m.gravesal@devbootcamp.com>
2015-08-28 15:48:23 -0400

    add card class tests and methods

--- source/flashcard_spec.rb
+++ source/flashcard_spec.rb
@@ -2,8 +2,26 @@ require_relative 'flashcards.rb'
 
 describe Parser  do
   describe "#get_cards_from_file" do
-    it "should parse each pair as aa array of hashes" do
+    it "should parse each pair as an array of hashes" do
       expect(Parser.get_cards_from_file("flashcard_samples.txt")).to be_a_kind_of(Array)
     end
+  end
+end
+
+describe Card do
+
+  card_info = {:definition => "Describe some crazy thing", :answer => "this is crazy"}
+
+  let(:card) { Card.new(card_info) }
+
+  describe "#initialize" do
+    it "should initialize as a kind of card" do
+      expect(card).to be_a_kind_of(Card)
+    end
+
+    it "it should have the proper attributes" do
+      expect(card).to have_attributes(:definition => "Describe some crazy thing", :answer => "this is crazy")
+    end
+
   end
 end
--- source/flashcards.rb
+++ source/flashcards.rb
@@ -1,23 +1,38 @@
 # Main file for
+class Card
+  attr_reader :definition, :answer
+
+  def initialize(cards = {})
+    @definition = cards[:definition]
+    @answer = cards[:answer]
+  end
+
+  def to_s
+    "#{definition} and #{answer}"
+  end
+
+  # def create_card
+  #   Parser.get_cards_from_file("flashcard_samples.txt")
+  # end
+end
+
 module Parser
   def self.get_cards_from_file(file)
     file = "flashcard_samples.txt"
-    array = []
+    card_array = []
     File.open(file, 'r')  do |file|
       file.each_line.each_slice(2) do |definition, answer|
-        array << {definition: definition, answer: answer}
+        card_array << {definition: definition, answer: answer}
       end
     end
-    array
+    card_array
   end
 end
 
 
-# File.open('path/to/file', 'r') do |file|
-#   # each_line gives an Enumerator object. On which I'm calling
-#   # each_slice to take 2 lines at a time, where first line is the
-#   # question, and the second one is the answer.
-#   file.each_line.each_slice(2).do |question, answer|
-#     array << {'Question' => question, 'Answer' => answer}
-#   end
-# end
+#to_s method will
+
+#puts Parser.get_cards_from_file("flashcard_samples.txt")
+# card_info = {:definition => "Describe some crazy thing", :answer => "this is crazy"}
+# test_card = Card.new(card_info)
+# puts test_card
