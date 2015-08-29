ae2eb04c13d723bd12efdb8d959dd083293fa5bc
LD & Domingo Moronta & Angelita Graves <pair+LDinho.dj-m.gravesal@devbootcamp.com>
2015-08-29 18:21:18 -0400

    organize files

--- source/parser.rb
+++ source/parser.rb
@@ -1,43 +1,30 @@
-# require_relative 'flashcards'
-# require 'byebug'
+require_relative 'flashcards'
 
-# module Parser
-# # Read the data and give it to us.
-#   # attr_reader :array
+module CreateCard
+# Create cards for us to use.
+  def self.create_card
+    card_hash = []
+    Parser.parse_file('flashcard_samples.txt').each_slice(2) do |definition, answer|
+      card_hash << {:definition => definition, :answer => answer}
+    end
+      newCard_array = []
+      card_hash.each do |card_object|
+        newCard_array << Card.new(card_object)
+      end
+      newCard_array
+  end
+end
 
-#   def self.parse_file(file)
-#     array = []
-#     File.open(file, 'r')  do |file|
-#       file.each_line do |row|
-#         array << row
-#       end
-#     end
-#     array
-#   end
-# end
+module Parser
 
-# module CreateCard
-# # Create cards for us to use.
-#   def self.create_card
-#     card_hash = []
-#     Parser.parse_file('flashcard_samples.txt').each_slice(2) do |definition, answer|
-#       card_hash << {:definition => definition, :answer => answer}
-#     end
-#       byebug
-#       newCard_array = []
-#       card_hash.each do |card_object|
-#         newCard_array << Card.new(card_object)
-#       end
-#       newCard_array
-#   end
+  def self.parse_file(file)
+    array = []
+    File.open(file, 'r')  do |file|
+      file.each_line do |row|
+        array << row
+      end
+    end
+    array
+  end
+end
 
-# end
-
-
-# puts CreateCard.create_card
-
-# # puts new_card = CreateCard.create_card
-# #Card.new(new_card)
-# #p new_card.class
-# # puts test = Card.new(new_card)
-# # puts
